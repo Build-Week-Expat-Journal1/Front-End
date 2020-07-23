a326fba2999b0bf6f97bcee8b56888e7fbf593a7
Ricardo Leite <56242343+ricardo-ljr@users.noreply.github.com>
2020-07-23 17:31:47 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,28 @@
 import React from 'react';
-<<<<<<< HEAD
 import { BrowserRouter as Router,Route, Switch } from 'react-router-dom'
 import './App.css';
 import Register from'./components/register';
 import Login from'./components/login';
 
+import './App.css';
+import FormCreateStoryPictures from "./component/FormCreateStoryPictures"
+import NavBar from "./component/NavBar"
+
 const App =() => {
 return (
-
+ <div className="App">
   <Router>
           <Switch>
-
+  
+              <FormCreateStoryPictures/>
+              <NavBar/>
               <Route exact path='/register' component={Register}/>    
               <Route  exact path='/login' component={Login}/>    
           </Switch>
  </Router>
+  </div>
 
 )
-
-=======
-import './App.css';
-<<<<<<< HEAD
-import FormCreateStoryPictures from "./component/FormCreateStoryPictures"
-=======
-import NavBar from "./component/NavBar"
->>>>>>> 52c740be5209c820ecff014dab0e55a2c8242c51
-
-function App() {
-  return (
-    <div className="App">
-<<<<<<< HEAD
-      <FormCreateStoryPictures/>
-     
-  
-=======
-      <NavBar/>
->>>>>>> 52c740be5209c820ecff014dab0e55a2c8242c51
-    </div>
-  );
->>>>>>> aa338782c43c13b4d021034ba4aedb8b1a5959ad
 }
 
 export default App;
