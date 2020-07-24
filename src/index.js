2b31ba87ff3c8b83ad86eec0adaba61f15a18ab1
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-07-23 21:15:25 -0400

    Imported Provider and createStore

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from "react-router-dom";
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//--------------//
+//    IMPORTS   //
+//--------------//
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import { rootReducer } from "./reducers/index";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+//---------------------//
+//    CREATE STORE     //
+//---------------------//
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
