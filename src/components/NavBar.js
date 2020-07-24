29e0891d233b22dded693944a786bac3715f0d03
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-07-23 20:49:29 -0400

    added login and register to component Route

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, Switch, Route } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <nav>
+        <h3>Expat Journal</h3>
+        <Link to="/">Landing</Link>
+        <Link to="/login">Sign In</Link>
+        <Link to="/register">Sign Up</Link>
+        <Link to="/Discover">Discover</Link>
+      </nav>
+      <Switch>
+                <Route exact path="/" component={}/>
+                <Route path="/SignIn" component={login}/>
+                <Route path="/SignUp" component={register}/>
+                <Route path="/Discover" component={}/>
+            </Switch>
+    </div>
+  );
+};
+
+export default NavBar;
