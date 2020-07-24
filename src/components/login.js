8d35a75eb78f8863ed7738d8e5766cf2c3c5bb71
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-07-23 21:34:44 -0400

    updating index

--- src/components/login.js
+++ src/components/login.js
@@ -1,50 +1,70 @@
-import React, { useState } from 'react'
+//------------------//
+//    IMPORTS       //
+//------------------//
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { userLogin } from "../actions";
 
+
+//---------------------//
+//   LOGIN COMPONENT   //
+//---------------------//
 const Login = () => {
-   const [user, setUser] = useState({
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-      email: '',
-      password: '',
+  const { email, password } = user;
 
-   });
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
-   const { email, password, } = user;
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login submit");
+  };
 
-   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+  return (
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary">Login</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Email Address</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
 
-   const onSubmit = e => {
-      e.preventDefault();
-      console.log('Login submit')
-
-   }
-
-   return (
-      <div className='form-container'>
-         <h1>
-            Account <span className='text-primary'>Login</span>
-         </h1>
-         <form onSubmit={onSubmit}>
-            <div className='form-group'>
-               <label htmlFor='name'>Email Address</label>
-               <input type='email' name='email' value={email} onChange={onChange} />
-
-            </div>
-            <div className='form-group'>
-               <label htmlFor='name'>Password</label>
-               <input type='password' name='password' value={password} onChange={onChange} />
-
-            </div>
-
-            <input type='submit' value='Login' className="btn btn-primary btn-block" />
-
-         </form>
-
-      </div>
-
-
-   )
+        <input
+          type="submit"
+          value="Login"
+          className="btn btn-primary btn-block"
+        />
+      </form>
+    </div>
+  );
+};
 
 
 
-}
-export default Login;
+const mapStateToProps = state => {
+   return ({
+
+      error: state.error, 
+
+   })
+};
+
+export default connect(
+   mapStateToProps, 
+   { userLogin } 
+)(login);
