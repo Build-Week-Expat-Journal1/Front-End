cd4df22c3b812b419e6e58a457f11dd5db1bfa28
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-07-24 22:35:12 -0400

    adding styling to match forms on nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,18 @@ import { Link, Switch, Route } from "react-router-dom";
 import login from "../components/login.js";
 import register from "../components/register";
 import FormCreateStoryPictures from "../components/FormCreateStoryPictures.js"
+import App from "../App.css"
 
 const NavBar = () => {
   return (
-    <div className="NavBar">
-      <nav>
-        <h3>Expat Journal</h3>
-        <Link to="/">Landing</Link>
-        <Link to="/Login">Log In</Link>
-        <Link to="/Register">Register</Link>
-        <Link to="/Discover">Discover</Link>
-        <Link to="/Contribute">Contribute</Link>
+    <div className="reactNavDiv">
+      <nav className="reactNav">
+        <h3 className="navTitle">Expat Journal</h3>
+        <Link className="navLink" to="/">Landing</Link>
+        <Link className="navLink"  to="/Login">Log In</Link>
+        <Link className="navLink"  to="/Register">Register</Link>
+        <Link className="navLink"  to="/Discover">Discover</Link>
+        <Link className="navLink"  to="/Contribute">Contribute</Link>
       </nav>
       <Switch>
                {/*} <Route exact path="/" component={}/> */}
