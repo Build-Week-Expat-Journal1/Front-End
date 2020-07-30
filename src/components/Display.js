34a32e5656e11f9872615083d024828d543d3c59
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-07-29 20:47:40 -0400

    data added to Display/Discovery page

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,25 @@
 import React from "react";
 import App from "../App.css";
 
-const Display = props => {
-  return <h2>{props.item.storyTitle}</h2>;
+const Display = (props) => {
+  return ( 
+
+<div className = "DataDisplay">
+
+  <img src ={props.img} />  
+  <h2>{props.storyTitle}</h2> 
+  <h3>Date written: {props.storyDate}</h3>
+  <p>{props.story}</p>
+  
+  
+  
+
+</div>
+
+  );
+
+
+  
 };
 
 export default Display;
