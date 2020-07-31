528bc2bab8546d777b04c22d1c6dbeb31d8dee90
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-07-31 16:14:41 -0400

    removed duplicate import, minor correction to createStoryReducer

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
-import register from "./components/register";
-import login from "./components/login";
-import FormCreateStoryPictures from "./components/FormCreateStoryPictures";
-import NavBar from "./components/NavBar";
-import DisplayMap from "./components/DisplayMap.js";
-import PrivateRoute from "./utils/PrivateRoute";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import register from './components/register';
+import login from './components/login';
+import FormCreateStoryPictures from './components/FormCreateStoryPictures';
+import NavBar from './components/NavBar';
+import DisplayMap from './components/DisplayMap.js';
+import PrivateRoute from './utils/PrivateRoute';
 
-import CreateStory from "./components/CreateStory";
+import CreateStory from './components/CreateStory';
 // import SlideShow from "./components/SlideShow";
 
-import "./App.css";
-import { CreateStory } from "./components/CreateStory";
+import './App.css';
 
 const App = () => {
-  const logout = (e) => {
+  const logout = e => {
     e.preventDefault();
-    localStorage.removeItem("token");
+    localStorage.removeItem('token');
     window.location.reload(false);
   };
 
   return (
-    <div className="App">
+    <div className='App'>
       {/* <button onClick={logout}>Log Out</button> */}
       <NavBar />
 
       <Switch>
-        <Route exact path="/Register" component={register} />
-        <Route exact path="/Login" component={login} />
+        <Route exact path='/Register' component={register} />
+        <Route exact path='/Login' component={login} />
         {/* <Route exact path="/" component={SlideShow} />  */}
 
         {/* These are the private Routes */}
         <PrivateRoute
           exact
-          path="/Contribute"
+          path='/Contribute'
           component={FormCreateStoryPictures}
         />
-        <PrivateRoute exact path="/Discover" component={DisplayMap} />
-        <PrivateRoute exact path="/CreateStory" component={CreateStory} />
+        <PrivateRoute exact path='/Discover' component={DisplayMap} />
+        <PrivateRoute exact path='/CreateStory' component={CreateStory} />
       </Switch>
     </div>
   );
